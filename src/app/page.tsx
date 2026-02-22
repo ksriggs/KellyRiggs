35ab1f7cec74cddadfa2661f42b37343b5813803
Stefan <stefischer@mail.com>
2026-02-22 13:57:47 -0500

    Replace manual Marquee with CompanyMarquee component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,15 @@ import { BookList } from '@/components/Books';
 
 import { GEA, Hubblle, PPG, UNFI, Watco } from '@/components/common';
 import { RecentPodcastEpisodes } from '@/components/Podcast';
+import CompanyMarquee from '@/components/CompanyMarquee';
 
 function Home() {
-
-    const fill = "#ffffff";
 
     return (
         <React.Fragment>
             <Jumbotron />
             <Layout main className="pt-40! pb-10 md:pb-20 gap-50 md:gap-60 z-30">
-                <Marquee gapBetween={20}>
-                    <div className="flex w-fit gap-x-20 opacity-50 justify-center items-center">
-                        <GEA height={200} width={200} fill={fill} />
-                        <Hubblle height={200} width={200} fill={fill} />
-                        <PPG height={150} width={150} fill={fill} />
-                        <UNFI height={200} width={200} fill={fill} />
-                        <Watco height={130} width={130} fill={fill} />
-                    </div>
-                </Marquee>
+                <CompanyMarquee />
                 <MyPromises />
                 <Testimonials />
                 <BookList />
