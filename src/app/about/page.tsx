55ff88aefc218dbeba39b706f663536f70898729
Stefan <stefischer@mail.com>
2026-02-22 13:56:35 -0500

    Populate About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,16 @@
+import { AboutHighlights, MeetKellyRiggs, WhatIDo } from '@/components/About';
 import { Layout } from '@/components/common';
 
 function About() {
 
     return(
         <Layout main transparent className="gap-50 mb-40 pt-40">
+            <div className="flex flex-col items-center justify-center gap-8">
+                <MeetKellyRiggs />
+                <WhatIDo className="flex flex-col lg:flex-row gap-10" />
+            </div>
+            <AboutHighlights />
+
         </Layout>
     );
 };
