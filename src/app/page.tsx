9f17cf064c195340d588cee9bf41154b30157f01
Stefan <stefischer@mail.com>
2026-02-23 14:26:15 -0500

    Move home page company marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,11 @@ function Home() {
         <React.Fragment>
             <Jumbotron />
             <Layout main className="pt-40! pb-10 md:pb-20 gap-50 md:gap-60 z-30">
-                <CompanyMarquee />
                 <MyPromises />
                 <RecentPodcastEpisodes />
                 <BookList />
+                <CompanyMarquee />
                 <Testimonials />
-
             </Layout>
         </React.Fragment>
     );
