494bf8154b552309d2ef6cb2ca1532f61ff90391
Stefan <stefischer@mail.com>
2026-03-03 11:49:47 -0500

    Add new constants to index

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,7 @@
 export { default as BREAKPOINTS } from './breakpoints';
+export * as CONTACT_INFO from './conactInfo';
 export * as ENV from './environment';
 export * as SOCIAL_LINKS from './socialLinks';
 export * as QUERY_KEYS from './queryKeys';
+export * as URLS from './urls';
+export * as YOUTUBE_VIDEO_IDS from './youtubeVideoIds';
