fd75dbf1573e01a000ed5008273474756c5b7d90
Stefan <stefischer@mail.com>
2026-03-03 11:50:53 -0500

    Update WhatIDo styling

--- src/components/About/WhatIDo.tsx
+++ src/components/About/WhatIDo.tsx
@@ -38,7 +38,7 @@ function WhatIDo({ className }: WhatIDoProps) {
         return items.map((item, index) => (
             <div 
                 key={`what-i-do-${item.title}-${index}`}
-                className="bg-card-light p-6 rounded-lg flex-1 flex flex-col gap-4"
+                className="bg-card-light p-6 rounded-lg flex-1 flex flex-col gap-4 text-center"
             >
                 <h1 className="font-bold text-2xl text-accent">{item.title}</h1>
                 <p className="font-semibold">
