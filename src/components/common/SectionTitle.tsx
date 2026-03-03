59c416cfd24a4dcb2a2b036eabcca65404d4f3b9
Stefan <stefischer@mail.com>
2026-03-03 11:51:43 -0500

    Force text color in SectionTitle

--- src/components/common/SectionTitle.tsx
+++ src/components/common/SectionTitle.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SectionTitle({ className, children }: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) {
 
     return(
-        <h1 className={`text-4xl lg:text-5xl font-bold text-center ${className}`}>
+        <h1 className={`text-4xl lg:text-5xl font-bold text-center text-text ${className}`}>
             {children}
         </h1>
     );
