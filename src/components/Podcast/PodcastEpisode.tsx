cd08053f1b8d4d7b41745a7202c0083174a9ed17
Stefan <stefischer@mail.com>
2026-03-03 11:52:56 -0500

    Adjust styling for PodcastEpisode

--- src/components/Podcast/PodcastEpisode.tsx
+++ src/components/Podcast/PodcastEpisode.tsx
@@ -14,14 +14,14 @@ function PodcastEpisode({ episode }: PodcastEpisodeProps) {
     const videoId = episode.id.split(":")[2];
 
     return(
-        <div className="bg-card-light rounded-lg w-full lg:h-120">
-            <div className="flex flex-col gap-8 pb-5">
-                <div className="rounded-t-lg overflow-hidden">
+        <div className="bg-card-light rounded-lg w-full lg:h-126">
+            <div className="flex flex-col gap-8 h-full">
+                <div className="rounded-t-lg overflow-hidden h-4/6">
                     <YouTubePlayer videoId={videoId} />
                 </div>
-                <div className="px-2 pb-4 flex flex-col gap-4">
+                <div className="px-2 flex flex-col gap-4 h-2/6 pb-4">
                     <h1 className="text-text font-semibold text-lg">{episode.title}</h1>
-                    <div className="flex gap-2 items-end">
+                    <div className="flex gap-2 items-end flex-1">
                         <FaRegCalendar className="text-secondary" />
                         <p className="text-accent font-semibold">
                             {dayjs(episode.published).format("MMMM D, YYYY")}
