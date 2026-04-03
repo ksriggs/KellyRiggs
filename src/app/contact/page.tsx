f9bba687b7f4fad9c1353f34343ba46031a13a2d
Stefan <stefischer@mail.com>
2026-04-03 12:03:54 -0400

    Reduce desktop section gap and add CTA component to Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,13 @@
 import { Layout } from '@/components/common';
 import { ContactContainer } from '@/containers';
+import CTA from '@/components/CTA';
 
 function Contact() {
 
     return(
-        <Layout main transparent className="gap-50 mb-40 pt-40">
+        <Layout main transparent className="gap-10 mb-40 pt-40">
+            <CTA />
             <div className="flex flex-col w-full items-center justify-center gap-10">
-                <h1 className="text-5xl font-bold font-beach">Contact</h1>
                 <ContactContainer />
             </div>
         </Layout>
