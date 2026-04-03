25af2f6d03a0df3daacdefbfaaed7c7f9d833a87
Stefan <stefischer@mail.com>
2026-04-03 10:06:57 -0400

    Update query keys and urls constants

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,1 +1,5 @@
 export const CALENDLY = "https://calendly.com/kellyriggs/30min";
+
+export const BASE_API_URL = "http://api.kellyriggs.com";
+export const REST_API_URL = `${BASE_API_URL}/wp-json/wp/v2`;
+export const GRAPHQL_API_URL = `${BASE_API_URL}/graphql`;
