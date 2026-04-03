62a01dc362f783be24c666e10892caa1aed78029
Stefan <stefischer@mail.com>
2026-04-03 10:08:23 -0400

    Refactor About components to use props instead of hard coded data

--- src/components/About/AboutHighlights.tsx
+++ src/components/About/AboutHighlights.tsx
@@ -1,19 +1,11 @@
 import { SectionTitle } from '../common';
 import { FaCheckCircle } from 'react-icons/fa';
 
-function AboutHighlights() {
+interface AboutHighlightsProps {
+    items: string[]
+};
 
-    const items: string[] = [
-        "Award-Winning co-author of Counter Mentor Leadership",
-        "Author of Quit Whining and Start SELLING! A Step-by-Step Guide to a Hall of Fame Career in Sales",
-        "Founder of the Business LockerRoom, Inc. (2006)",
-        "Member of the prestigious Forbes' Coaches Council",
-        "In-demand platform keynote speaker",
-        "Former 2-Time National Salesperson-of-the-Year",
-        "Former Corporate Director of Sales Development",
-        "Successful Entrepreneur",
-        "Manager and lead trainer in 2 corporate sales training programs"
-    ];
+function AboutHighlights({ items }: AboutHighlightsProps) {
 
     const renderItems = () => {
         return items.map((highlight, index) => (
--- src/components/About/WhatIDo.tsx
+++ src/components/About/WhatIDo.tsx
@@ -1,38 +1,14 @@
 import React from 'react';
 
 interface WhatIDoProps {
-    className?: React.HTMLAttributes<HTMLDivElement>["className"]
+    className?: React.HTMLAttributes<HTMLDivElement>["className"],
+    items: {
+        title: string,
+        content: string
+    }[]
 };
 
-function WhatIDo({ className }: WhatIDoProps) {
-
-    const items = [
-        { 
-            title: "What I Do",
-            content: `
-                I teach others how to compete and win in both sales and corporate 
-                leadership. If you're tired of being stuck or being average, my talent is identifying 
-                the core issues and teaching you exactly what you need to do to overcome the problems.
-            `
-        },
-        { 
-            title: "What Makes Me Different",
-            content: `
-                I've been a No. 1 salesperson nationally, I've owned four 
-                businesses, and I've had spectacular success as a coach and consultant. Very rarely 
-                will you find all three of those components in one individual - someone who has done 
-                it personally, taught others, and helped them successfully implement it.
-            `
-        },
-        { 
-            title: "Why It Works",
-            content: `
-                When you partner with me, you don't waste your time or your money on theory, 
-                philosophy, or the same old cliches. You get the most efficient, effective, and affordable 
-                performance coaching that executives and business owners are looking for right now.
-            `
-        },
-    ];
+function WhatIDo({ className, items }: WhatIDoProps) {
 
     const renderItems = () => {
         return items.map((item, index) => (
