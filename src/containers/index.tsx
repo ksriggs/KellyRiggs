46f42251a85984490bd76dcfba5161f8244d5d00
Stefan <stefischer@mail.com>
2026-04-12 11:09:12 -0400

    Create BooksContainer component

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,3 +1,4 @@
 export { default as AboutContainer } from './AboutContainer';
+export { default as BooksContainer } from './BooksContainer';
 export { default as CoachingContainer } from './CoachingContainer';
 export { default as ContactContainer } from './ContactContainer';
