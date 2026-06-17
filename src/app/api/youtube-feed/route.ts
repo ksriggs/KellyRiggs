cf88120088c8548fe84d11be61c32cd2c593f828
Stefan <stefischer@mail.com>
2026-06-17 11:24:56 -0400

    Add error handling to youtube-feed internal api endpoint and add site url environment

--- src/app/api/youtube-feed/route.ts
+++ src/app/api/youtube-feed/route.ts
@@ -2,15 +2,22 @@ import { NextResponse } from "next/server";
 import { XMLParser } from "fast-xml-parser";
 
 export async function GET() {
-    const res = await fetch(
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCz6c3blD3UdfjYP-8NPhDlA",
-        { next: { revalidate: 3600 } }
-    );
+    try {
+        const res = await fetch(
+            "https://www.youtube.com/feeds/videos.xml?channel_id=UCz6c3blD3UdfjYP-8NPhDlA",
+            { next: { revalidate: 3600 } }
+        );
 
-    const xml = await res.text();
+        const xml = await res.text();
 
-    const parser = new XMLParser();
-    const parsed = parser.parse(xml);
+        const parser = new XMLParser();
+        const parsed = parser.parse(xml);
 
-    return NextResponse.json({ ...parsed });
+        return NextResponse.json({ ...parsed });
+    }
+    catch(err) {
+        console.error("Error fetching YouTube Feed: ", err);
+        return NextResponse.json({ error: "Failed to fetch" }, { status: 500 });
+    }
+    
 };
--- src/services/internal.ts
+++ src/services/internal.ts
@@ -1,7 +1,9 @@
 import type { YouTubeFeedResponse } from '@/types/youtubeFeed';
 
+import { ENV } from '@/constants';
+
 export async function getYouTubeRecentYouTubeVideos(): Promise<YouTubeFeedResponse> {
-    const res = await fetch("/api/youtube-feed");
+    const res = await fetch(`${ENV.getSiteUrl()}/api/youtube-feed`);
 
     if(!res.ok) {
         throw new Error("Unable to fetch YouTube Feed");
